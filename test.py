5755167392946960b802ae0e5aed788ded44df4d
Mohan <snowmohan2@gmail.com>
2017-11-26 21:57:16 -0500

    1

---
+++ test.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[31]:
+
+
+import os
+iperf = os.system("iperf3 -c node-3  &")
+val = os.popen('ping localhost -c 10').read()
+a = val.split('/')
+avg = a[-3]
+f = open("a.txt", "a")
+#f.read()
+f.write(avg+'\n')
+os.system('killall iperf3')
+f.close()
+
