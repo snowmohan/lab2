b60f350afd034d436a0c1ef6ac3989f45c9993df
snowmohan <252203262@qq.com>
2017-11-27 00:00:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import os
 import time
-for i in range(1,102,10):
+for i in range(1,120,10):
     thr = str(i)
-    cmd = 'iperf3 -c node-3 '+'-b '+thr+'Mb'+' -t 240'+' &'
+    cmd = 'iperf3 -c node-3 '+'-b '+thr+'Mb'+' -t 300'+' &'
     iperf = os.system(cmd)
     time.sleep(5)
-    val = os.popen('ping node-3 -c 60').read()
+    val = os.popen('ping node-3 -c 240').read()
     a = val.split('/')
     avg = a[-3]
     f = open("a.txt", "a")
-    f.write(avg+' '+thr+'\n')
+    f.write(thr+' '+avg+'\n')
     os.system('killall iperf3')
     f.close()
