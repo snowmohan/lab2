ad45e5219f105c521ccda9714242acc724a55d43
snowmohan <252203262@qq.com>
2017-11-26 22:16:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
-
-# coding: utf-8
-
-# In[31]:
-
-
 import os
-iperf = os.system("iperf3 -c node-3  &")
-val = os.popen('ping node-3 -c 10').read()
-a = val.split('/')
-avg = a[-3]
-f = open("a.txt", "a")
-#f.read()
-f.write(avg+'\n')
-os.system('killall iperf3')
-f.close()
-
+for i in range(1,4):
+    thr = str(i)
+    cmd = 'iperf3 -c node-3 '+'-b '+thr+'Mb'
+    iperf = os.system(cmd)
+    val = os.popen('ping localhost -c 10').read()
+    a = val.split('/')
+    avg = a[-3]
+    f = open("a.txt", "a")
+    f.write(avg+thr'\n')
+    os.system('killall iperf3')
+    f.close()
