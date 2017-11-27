c2a5b7e168f6a33df5d06eb3d968c6627fd6a2d4
snowmohan <252203262@qq.com>
2017-11-27 11:30:59 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import os
 import time
-for i in range(1,97):
+for i in range(1,40):
     thr = str(i)
     cmd = 'iperf3 -c node-3 '+'-b '+thr+'Mb'+' -t 300'+' &'
     iperf = os.system(cmd)
     time.sleep(5)
-    val = os.popen('ping node-3 -i 0.2 -c 1200').read()
+    val = os.popen('ping node-3 -i 0.2 -c 300').read()
     a = val.split('/')
     avg = a[-3]
     f = open("a.txt", "a")
