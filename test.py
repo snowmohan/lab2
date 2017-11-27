2602c1d6524ea1c20f52da62f0619d66465ea27c
snowmohan <252203262@qq.com>
2017-11-26 22:35:28 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ for i in range(50,53):
     cmd = 'iperf3 -c node-3 '+'-b '+thr+'Mb'+' -t 240'+' &'
     iperf = os.system(cmd)
     time.sleep(10)
-    val = os.popen('ping node-3 -c 25').read()
+    val = os.popen('ping node-3 -c 200').read()
     a = val.split('/')
     avg = a[-3]
     f = open("a.txt", "a")
