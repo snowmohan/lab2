bdbc09d27998f98d6a6788e08bc115fdd829e0a5
snowmohan <252203262@qq.com>
2017-11-26 21:59:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@
 
 import os
 iperf = os.system("iperf3 -c node-3  &")
-val = os.popen('ping localhost -c 10').read()
+val = os.popen('ping node-3 -c 10').read()
 a = val.split('/')
 avg = a[-3]
 f = open("a.txt", "a")
